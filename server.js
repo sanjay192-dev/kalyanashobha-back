4f75641d901b916aed66bb40cb527238a9841047
sanjay192-dev <adepusanjay192@gmail.com>
2026-02-15 09:57:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const PaymentInterest = require('./models/PaymentInterest');
 const Admin = require('./models/Admin');
 
 const app = express();
-app.use(cors();
+app.use(cors());
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
