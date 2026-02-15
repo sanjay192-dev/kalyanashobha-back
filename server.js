c1796062e05cac99c2944927b0f8e7ec806b39db
sanjay192-dev <adepusanjay192@gmail.com>
2026-02-15 11:31:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,10 +21,9 @@ const Admin = require('./models/Admin');
 
 const app = express();
 app.use(cors({
-    origin: [
-        "http://localhost:5173",            // Localhost
-        "https://kalyanashobha.vercel.app"  // Production Domain
-    ]
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
