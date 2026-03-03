8a2d847cc7e669b2a633a4ce560d2e3990f64b4e
sanjay192-dev <adepusanjay192@gmail.com>
2026-03-03 14:55:09 +0530

    Create Otp.js

---
+++ models/Otp.js
@@ -0,0 +1,20 @@
+// models/Otp.js
+const mongoose = require('mongoose');
+
+const otpSchema = new mongoose.Schema({
+    email: { 
+        type: String, 
+        required: true 
+    },
+    otp: { 
+        type: String, 
+        required: true 
+    },
+    createdAt: { 
+        type: Date, 
+        default: Date.now, 
+        expires: 300 // MongoDB will automatically delete this document after 300 seconds (5 minutes)
+    }
+});
+
+module.exports = mongoose.model('Otp', otpSchema);
