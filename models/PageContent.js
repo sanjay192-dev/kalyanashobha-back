8b5243a6e3b1835fde5940f4ef91852c75f304f0
sanjay192-dev <adepusanjay192@gmail.com>
2026-03-07 07:50:36 +0530

    Create PageContent.js

---
+++ models/PageContent.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const pageContentSchema = new mongoose.Schema({
+    pageName: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        enum: ['terms', 'refund', 'about'] // Restricts to these specific pages
+    },
+    content: { 
+        type: String, // You can store plain text or HTML here (from a Rich Text Editor)
+        required: true 
+    },
+    lastUpdatedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Admin'
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('PageContent', pageContentSchema);
