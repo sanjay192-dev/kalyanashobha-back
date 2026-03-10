f3de2cf532040ee2d972fad358666ef7c8bc44fa
sanjay192-dev <adepusanjay192@gmail.com>
2026-03-10 15:10:09 +0530

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const AdminSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // Will be hashed
+  password: { type: String, required: true }, 
   role: { type: String, enum: ['SuperAdmin', 'Moderator'], default: 'SuperAdmin' },
+  // NEW: Add a permissions array to store the allowed pages
+  permissions: { type: [String], default: [] }, 
   createdAt: { type: Date, default: Date.now }
 });
 
