9d9086e24c866844b3c269706a61655de9a825ee
sanjay192-dev <adepusanjay192@gmail.com>
2026-03-10 15:10:39 +0530

    Update CommunityModel.js

--- models/CommunityModel.js
+++ models/CommunityModel.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 
 const CommunitySchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true, trim: true },
-  subCommunities: [{ type: String, trim: true }]
+  subCommunities: [{ type: String, trim: true }],
+  order: { type: Number, default: 0 } // <-- ADD THIS FIELD
 });
 
 module.exports = mongoose.model('Community', CommunitySchema);
