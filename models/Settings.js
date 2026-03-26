f2569bc80e9ff03d0a2b9f1a15cb09aa94b4cbde
sanjay192-dev <adepusanjay192@gmail.com>
2026-03-26 07:39:41 +0530

    Update Settings.js

--- models/Settings.js
+++ models/Settings.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const SettingsSchema = new mongoose.Schema({
     maleRegistrationFee: { type: Number, default: 0 },
     femaleRegistrationFee: { type: Number, default: 0 },
+    upiId: { type: String }, // Added dynamic UPI ID field
     lastUpdatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin' }
 }, { timestamps: true });
 
